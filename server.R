d17a1093ba3ce91e8278e93389d8a67fcbb22c2f
Matt Strimas-Mackey <mstrimas@gmail.com>
2016-08-02 12:15:01 -0700

    tabbed layout

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ shinyServer(function(input, output, session) {
   fresh_data <- TRUE
   
   ##########   Rattle Map   ########## 
+  
   # data
   rattles <- eventReactive(input$submit, {
     fresh_data <<- TRUE
@@ -10,6 +11,7 @@ shinyServer(function(input, output, session) {
                      data = TRUE)
   })
   reverse_grid <- eventReactive(input$submit, {input$grid_input == "AG"})
+  
   # filters for rattle data frame
   # date
   output$date_input <- renderUI({
@@ -21,10 +23,40 @@ shinyServer(function(input, output, session) {
     dateRangeInput("date_input", "Dates: ",
                    start = dates[1], end = dates[2],
                    min = dates[1], max = dates[2],
-                   separator= "-")
+                   separator = "-")
   })
-  # reactive expression for data range to filter
+  # loc x
+  output$locx_input <- renderUI({
+    if (is.data.frame(rattles()) & nrow(rattles()) > 0) {
+      locs <- range(rattles()$x)
+    } else {
+      locs <- c(0, 22)
+    }
+    locs[1] <- floor(locs[1])
+    locs[2] <- ceiling(locs[2])
+    sliderInput("locx_input", "Loc X: ", 
+                min = locs[1], max = locs[2],
+                value = locs,
+                step = 1)
+  })
+  # loc y
+  output$locy_input <- renderUI({
+    if (is.data.frame(rattles()) & nrow(rattles()) > 0) {
+      locs <- range(rattles()$y)
+    } else {
+      locs <- c(0, 22)
+    }
+    locs[1] <- floor(locs[1])
+    locs[2] <- ceiling(locs[2])
+    sliderInput("locy_input", "Loc Y: ", 
+                min = locs[1], max = locs[2],
+                value = locs,
+                step = 1)
+  })
+  
+  # reactive expressions for filters
   # required because web page inputs not updated until after full flush
+  # date
   date_range <- reactive({
     r <- rattles()
     i <- input$date_input
@@ -43,13 +75,51 @@ shinyServer(function(input, output, session) {
     }
     return(dates)
   })
+  # loc x
+  locx_range <- reactive({
+    r <- rattles()
+    i <- input$locx_input
+    # if new data have just been pulled reference data frame directly
+    if (is.data.frame(r) & nrow(r) & fresh_data) {
+      locs <- range(r$x)
+      locs[1] <- floor(locs[1])
+      locs[2] <- ceiling(locs[2])
+    } else if (!is.null(i)) {
+      locs <- i
+    } else {
+      locs <- NULL
+    }
+    return(locs)
+  })
+  # loc y
+  locy_range <- reactive({
+    r <- rattles()
+    i <- input$locy_input
+    # if new data have just been pulled reference data frame directly
+    if (is.data.frame(r) & nrow(r) & fresh_data) {
+      locs <- range(r$y)
+      locs[1] <- floor(locs[1])
+      locs[2] <- ceiling(locs[2])
+    } else if (!is.null(i)) {
+      locs <- i
+    } else {
+      locs <- NULL
+    }
+    return(locs)
+  })
+  
   # filtered data
   rattles_filtered <- reactive({
-    # next line required to ensure reactive dependence on date_range()
+    # next 3 lines required to ensure reactive dependence on ranges
     dates <- date_range()
-    if (is.data.frame(rattles()) & nrow(rattles()) > 0 & !is.null(dates)) {
+    locx <- locx_range()
+    locy <- locy_range()
+    if (is.data.frame(rattles()) & nrow(rattles()) > 0 & 
+        !is.null(dates) & !is.null(locx) & !is.null(locy)) {
       r <- rattles() %>% 
-        filter(date >= dates[1], date <= dates[2])
+        filter(date >= dates[1], date <= dates[2],
+               x >= locx[1], x <= locx[2],
+               y >= locy[1], y <= locy[2])
       if (nrow(r) == 0) {
         r <- NULL
       }
@@ -58,28 +128,47 @@ shinyServer(function(input, output, session) {
     }
     return(r)
   })
+  
   # plot
   observe({
-    #if (!is.null(date_range())) {
-      if (!is.null(rattles_filtered())) {
-        rattles_filtered() %>% 
-          krsp:::plot_rattles(reverse_grid()) %>% 
-          bind_shiny("rattlemap_plot")
-      } else {
-        message_plot("No rattles found.") %>%
-          bind_shiny("rattlemap_plot")
-      }
-      # ui now updated, data no longer fresh
-      fresh_data <<- FALSE
-    #}
+    if (!is.null(rattles_filtered())) {
+      
+      rattles_filtered() %>% 
+        krsp:::plot_rattles(reverse_grid()) %>% 
+        bind_shiny("rattlemap_plot")
+    } else {
+      message_plot("No rattles found.") %>%
+        bind_shiny("rattlemap_plot")
+    }
+    # ui now updated, data no longer fresh
+    fresh_data <<- FALSE
   })
+  
   # data table
   output$rattlemap_table = DT::renderDataTable(
-    rattles_filtered(),
-    server = FALSE, 
+    if (!is.null(rattles_filtered())) {
+      rattles_filtered() %>% select(-id)
+    } else {
+      NULL
+    },
+    server = TRUE,
     options = list(pageLength = 10, autoWidth = TRUE),
-    rownames = FALSE
+    class = 'cell-border stripe',
+    rownames = FALSE,
+    colnames = c(
+      "Squirrel ID" = "squirrel_id",
+      "Loc X" = "x",
+      "Loc Y" = "y",
+      "Grid" = "grid",
+      "Sex" = "sex",
+      "Colours" = "colours",
+      "Tags" = "tags", 
+      "Rattle Date" = "date",
+      "Last Trapped" = "trap_date"),
+    filter = "top"
   )
+  
+  # download
   output$download_data <- downloadHandler(
     filename = function() {
       paste0("rattles-",
@@ -88,7 +177,12 @@ shinyServer(function(input, output, session) {
              ".csv")
     },
     content = function(file) {
-      write_csv(rattles_filtered(), file)
+      data <- rattles_filtered()
+      validate(
+        need(is.data.frame(data) & nrow(data) > 0, 
+             "No data to download")
+      )
+      write_csv(data, file)
     }
   )
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,43 @@
-shinyUI(fluidPage(
+shinyUI(navbarPage(
   
-  # titel panel
-  titlePanel("Squirrel Dashboard"),
+  # title
+  "Squirrel Dashboard",
   
-  # use sidebar layout
-  sidebarLayout(
-    
-    # sidebar
-    sidebarPanel(
-      h1("Rattle Map"),
-      "Display a map of rattles from behaviour and trapping records.",
-      h3("Grid and year: "),
-      fluidRow(
-        column(6, selectInput("grid_input", NULL, grids)),
-        column(6, selectInput("year_input", NULL, years))
+  ##########   Rattle Map   ########## 
+  tabPanel("Rattle Map",
+    sidebarLayout(
+      sidebarPanel(
+        h1("Rattle Map"),
+        "Display a map of rattles from behaviour and trapping records.",
+        h3("Grid and year: "),
+        fluidRow(
+          column(6, selectInput("grid_input", NULL, grids)),
+          column(6, selectInput("year_input", NULL, years))
+        ),
+        actionButton("submit", "Submit"),
+        conditionalPanel(condition = "input.submit > 0",
+          h3("Filter"),
+          uiOutput("date_input"),
+          uiOutput("locx_input"),
+          uiOutput("locy_input"),
+          downloadButton('download_data', 'Download')
+        ),
+        width = 3
       ),
-      actionButton("submit", "Submit"),
-      conditionalPanel(condition = "input.submit > 0",
-        h3("Filter"),
-        uiOutput("date_input"),
-        downloadButton('download_data', 'Download')
-      ),
-      width = 3
-    ),
-    
-    # main panel
-    mainPanel(
-      ggvisOutput("rattlemap_plot")
+      
+      # main panel
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Map", ggvisOutput("rattlemap_plot")),
+          tabPanel("Table", DT::dataTableOutput("rattlemap_table"))
+        )
+      )
     )
   ),
-  DT::dataTableOutput("rattlemap_table")
+  
+  ##########   Progress   ########## 
+  tabPanel("Progress"),
+  
+  ##########   Error Checking   ########## 
+  tabPanel("Error Checking")
 ))
